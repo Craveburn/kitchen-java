a8028e8faad80d49f23f6685a72599204533dbff
Cameron Rabunn <raburncameron@gmail.com>
2018-03-29 18:32:21 -0600

    adding this for future reference

---
+++ kitchen.js
@@ -0,0 +1,57 @@
+
+
+'use strict'
+
+
+class Kitchen {
+    constructor(microwave, oven) {
+        this.microwave = microwave
+        this.oven = oven
+    }
+}
+
+class Microwave {
+    constructor(hasPower, nuking) {
+        this.hasPower = hasPower
+        this.nuking = nuking
+    }
+    quickCook(){ 
+        this.nuking = this.nuking
+        return this.nuking
+}
+
+    switchPower() {
+        this.hasPower = !this.hasPower
+
+        return this.hasPower
+    }
+}
+
+class Oven {
+    constructor(hasPower, burning){
+        this.hasPower = hasPower
+        this.burning = burning
+    }
+
+    slowCook(){
+        this.burning = this.burning
+        return this.burning
+    }
+
+    switchPower() {
+        this.hasPower = !this.hasPower
+
+        return this.hasPower
+    }
+}
+
+const microwave = new Microwave(true, 'Nuking')
+const oven = new Oven(false, 'Burning')
+
+microwave.switchPower()
+microwave.quickCook()
+oven.switchPower()
+oven.slowCook()
+
+console.log('Does My Microwave Have Power', microwave.hasPower, 'Is it cooking?', microwave.quickCook())
+console.log('Does My Oven Have Power?', oven.hasPower, 'Is it Cooking?', oven.slowCook())
